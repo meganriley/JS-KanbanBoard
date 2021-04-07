2128a3022cdadb2f409f12e5de1edba4bf2d3466
Meg <meganriley806@gmail.com>
2021-04-07 18:26:56 -0500

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -34,7 +34,14 @@ const create_item = () => {
   save_btn.innerHTML = 'Save';
   item.append(input);
   save_btn.addEventListener('click', () => {
-    
+    error.innerHTML = '';
+    if (input.value != '') {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
   })
 };
 
