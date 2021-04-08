81d0f3d73a24c9ff99937abb823df6f7e0b7eb01
Meg <meganriley806@gmail.com>
2021-04-08 07:16:49 -0500

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -48,14 +48,14 @@ const create_item = () => {
     return item;
 };
 
-document.querySelectorAll('.drop').forEach(element => {
-  element.addEventListener('drop', event => {
-    event.preventDefault();
-    const id = event.dataTransfer.getData('text');
-    event.target.appendChild(document.getElementById(id));
-    //element.addEventListener('dragover', (event) => {
-      console.log("You dragged me!")
-      event.preventDefault();
-    })
-  })
-});
+// document.querySelectorAll('.drop').forEach(element => {
+//   element.addEventListener('drop', event => {
+//     event.preventDefault();
+//     const id = event.dataTransfer.getData('text');
+//     event.target.appendChild(document.getElementById(id));
+//     element.addEventListener('dragover', (event) => {
+//       console.log("You dragged me!")
+//       event.preventDefault();
+//     })
+//   })
+// });
